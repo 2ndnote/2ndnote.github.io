05f54691109cebf87b27d1166b83d907c2cba06a
ey1031 <eyudeveloper@gmail.com>
2018-01-20 13:26:03 -0800

    replicate header

--- 2ndnote/src/index.js
+++ 2ndnote/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import Navbar from './nav'
 import registerServiceWorker from './registerServiceWorker';
 
+ReactDOM.render(<Navbar/>, document.getElementById('nav'));
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
---
+++ 2ndnote/src/nav.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './bootstrap.css'
+import './nav.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div class="container">
+      <div class="navbar-header">
+        <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon icon-bar"></span>
+          <span class="icon icon-bar"></span>
+          <span class="icon icon-bar"></span>
+        </button>
+        <a href="#" class="navbar-brand">2NDNOTE</a>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#home" class="smoothScroll">HOME</a></li>
+          <li><a href="#mission" class="smoothScroll">OUR MISSION</a></li>
+          <li><a href="#team" class="smoothScroll">ABOUT US</a></li>
+          <li><a href="#contact" class="smoothScroll">CONTACT</a></li>
+        </ul>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
